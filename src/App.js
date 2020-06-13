491c64b6fdc5a874456209064414a7a124cc5fb1
user <koseyo08@yahoo.co.jp>
2020-06-14 07:42:37 +0900

    created react-redux

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from "react";
-import logo from "./logo.svg";
+import { Provider } from "react-redux"
 import "./App.css";
+import store from "./redux/store"
 
 const App = () => {
   return (
+    <Provider store={store}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <h1>Setup React Redux</h1>
     </div>
+    </Provider>
   );
 };
 
