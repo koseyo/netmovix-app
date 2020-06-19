a6484bb94af0cea2302399731bc182c351acf689
user <koseyo08@yahoo.co.jp>
2020-06-20 06:17:37 +0900

    added grid and rating

--- src/components/content/main-content/MainContent.js
+++ src/components/content/main-content/MainContent.js
@@ -2,9 +2,35 @@ import React, { useState } from "react";
 import "./MainContent.scss";
 import SlideShow from "../slide-show/SlideShow";
 import Pagination from "../pagination/Pagination";
+import Grid from "../grid/Grid";
 
 const MainContent = () => {
-  const images = [1, 2, 3, 4, 5]; // imageAPI
+  const images = [
+    {
+      url: "https://images.pexels.com/photos/688574/pexels-photo-688574.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      rating: 7.5,
+    },
+    {
+      url: "https://images.pexels.com/photos/688574/pexels-photo-688574.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      rating: 7.5,
+    },
+    {
+      url: "https://images.pexels.com/photos/776653/pexels-photo-776653.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      rating: 7.5,
+    },
+    {
+      url: "https://images.pexels.com/photos/776653/pexels-photo-776653.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      rating: 7.5,
+    },
+    {
+      url: "https://images.pexels.com/photos/255379/pexels-photo-255379.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      rating: 7.5,
+    },
+    {
+      url: "https://images.pexels.com/photos/255379/pexels-photo-255379.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      rating: 7.5,
+    },
+  ]; // imageAPI
   const [currentPageIndex, setCurrentPageIndex] = useState(1);
   const paginate = (type) => {
     if (type === "prev" && currentPageIndex > 1) {
@@ -17,12 +43,12 @@ const MainContent = () => {
     <div className="main-content">
       <SlideShow images={images} auto={true} arrows={true} />
       <div className="movie-title">
-        <div className="movie-type">movie-type</div>
+        <div className="movie-type"></div>
         <div className="movie-page">
           <Pagination paginate={paginate} currentPageIndex={currentPageIndex} totalPageIndex={10} />
         </div>
       </div>
-      {/* Display-grid-component */}
+      <Grid images={images} />
     </div>
   );
 };
